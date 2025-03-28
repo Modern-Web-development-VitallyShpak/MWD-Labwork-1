5b289c257de05e910fd3751a37155ad43e4625a5
vspak <vspak@LAPTOP-RBKK69EM>
2025-03-28 15:09:01 +0300

    feat: add src\presenter\tasks-board-presenter.js for task board rendering
    - Add tasks-board-presenter.js to handle task board rendering logic
    - Include related model (task-model.js) and mock data (task.js)
    - Prepare tasks for rendering in corresponding board sections
    - Maintain existing component interfaces for backward compatibility

--- src/main.js
+++ src/main.js
@@ -1,39 +1,19 @@
 import HeaderComponent from './view/header-component.js';
 import AddTaskFormComponent from './view/add-task-form-component.js';
-import BoardTaskComponent from './view/boardtask-component.js';
-import TaskListComponent from './view/task-list-component.js';
 import { render, RenderPosition } from './framework/render.js';
+import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
+import { tasks } from './mock/task.js'; // Импортируем задачи из файла
+import TaskModel from './model/task-model.js';
 
 const bodyContainer = document.querySelector('body');
 const boardAppInnerContainer = document.querySelector('.board-app__inner');
 
-render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
-
+const headerComponent = new HeaderComponent();
 const formComponent = new AddTaskFormComponent();
+
+render(headerComponent, bodyContainer, RenderPosition.AFTERBEGIN);
 render(formComponent, boardAppInnerContainer);
 
-const boardComponent = new BoardTaskComponent();
-render(boardComponent, boardAppInnerContainer);
-
-const backlogList = new TaskListComponent();
-const inProgressList = new TaskListComponent();
-const doneList = new TaskListComponent();
-const trashList = new TaskListComponent();
-
-const taskboardElement = boardComponent.getElement();
-taskboardElement.querySelector('.column-backlog .tasks-list').append(backlogList.getElement());
-taskboardElement.querySelector('.column-in-progress .tasks-list').append(inProgressList.getElement());
-taskboardElement.querySelector('.column-done .tasks-list').append(doneList.getElement());
-taskboardElement.querySelector('.column-trash .tasks-list').append(trashList.getElement());
-
-backlogList.addTask('Выучить JS', 'backlog');
-backlogList.addTask('Выучить React', 'backlog');
-
-inProgressList.addTask('Выпить смузи', 'in-progress');
-inProgressList.addTask('Попить воды', 'in-progress');
-
-doneList.addTask('Позвонить маме', 'done');
-doneList.addTask('Погладить кота', 'done');
-
-trashList.addTask('Сходить погулять', 'trash');
-trashList.addTask('Прочитать Войну и Мир', 'trash');
+const taskModel = new TaskModel(tasks);
+const tasksBoardPresenter = new TasksBoardPresenter(boardAppInnerContainer, taskModel);
+tasksBoardPresenter.init();
